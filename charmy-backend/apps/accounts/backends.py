44ced3f20f0207bb683f9a8d042d7c2e20825ac2
Juao20 <juao.aifa@epitech.eu>
2026-05-24 18:43:38 +0100

    fix: admin connexion info changes

--- charmy-backend/apps/accounts/backends.py
+++ charmy-backend/apps/accounts/backends.py
@@ -6,8 +6,15 @@ User = get_user_model()
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
+            # Essaie d'abord par email
             user = User.objects.get(email=username)
-            if user.check_password(password):
-                return user
         except User.DoesNotExist:
-            return None
+            try:
+                # Sinon essaie par username
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None

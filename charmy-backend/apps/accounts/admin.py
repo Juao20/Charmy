d11beda1077d25247ad8be4d257d55bd42ff06c4
Juao20 <juao.aifa@epitech.eu>
2026-09-02 21:26:08 +0100

    recommit

--- charmy-backend/apps/accounts/admin.py
+++ charmy-backend/apps/accounts/admin.py
@@ -6,4 +6,3 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ["email", "is_premium", "premium_until", "created_at"]
     search_fields = ["email"]
-    ordering = ["-created_at"]
